7d0b2f788673f68167c656be5a7a94f1aae82a67
kah jospen <jospenngumk@gmail.com>
2023-06-24 11:01:30 +0100

    update

--- config/appconfig.js
+++ config/appconfig.js
@@ -2,7 +2,7 @@ require('dotenv').config;
 
 const MONGO_DB_CONFIG = {
 
-  DB: 'mongodb+srv://jospenngumk:pIl7qeDoj7LkRQVL@cluster0.4kgmagk.mongodb.net/ReactFrontendProject?retryWrites=true&w=majority'
+  DB: 'mongodb+srv://jospenngumk:VKmqxxp11Bip1BjG@cluster0.4kgmagk.mongodb.net/?retryWrites=true&w=majority'
 } 
 module.exports = {
   MONGO_DB_CONFIG
